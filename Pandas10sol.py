47a1623ea6c93ac3fa3590fd0ce152ab81f63712
Pooja Madhup <90482825+pooja9050@users.noreply.github.com>
2024-05-27 16:22:05 -0700

    Update Pandas10sol.py

--- Pandas10sol.py
+++ Pandas10sol.py
@@ -43,6 +43,26 @@ def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
 import pandas as pd
 
 def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
+    mydictionary = {}
+    for i in range (len(daily_sales)):
+        d_id = daily_sales['date_id'][i]
+        m_name = daily_sales['make_name'][i]
+        l_id = daily_sales['lead_id'][i]
+        p_id = daily_sales['partner_id'][i]
+        if(d_id, m_name) not in mydictionary:
+            mydictionary[(d_id, m_name)] = [set(),set()]
+        mydictionary[(d_id, m_name)][0].add(l_id)
+        mydictionary[(d_id, m_name)][1].add(p_id)
+    result = []
+    for key, value in mydictionary.items():
+        result.append([key[0],key[1], len(value[0]), len(value[1])])
+    return pd.DataFrame(result, columns = ['date_id', 'make_name', 'unique_leads','unique_partners'])
+
+
+
+import pandas as pd
+
+def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
     df = daily_sales.groupby(['date_id','make_name']).agg({
         'lead_id' : pd.Series.nunique,
         'partner_id' : pd.Series.nunique
@@ -73,4 +93,28 @@ def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
     
     return grouped
 
+#1050. Actors and Directors Who Cooperated At Least Three Times
+import pandas as pd
+
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    df = actor_director.groupby(['actor_id','director_id']).size().reset_index(name = 'counts')
+    df = df[df['counts'] >= 3]
+    return df[['actor_id', 'director_id']]
+
+
+import pandas as pd
+
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    mydictionary = {}
+    for i in range (len(actor_director)):
+        a_id = actor_director['actor_id'][i]
+        d_id = actor_director['director_id'][i]
+        if (a_id, d_id) not in mydictionary:
+            mydictionary[(a_id, d_id)] = 0
+        mydictionary[(a_id, d_id)] += 1
+    result = []
+    for key, value in mydictionary.items():
+        if value >=3:
+            result.append(key)
+    return pd.DataFrame(result, columns = ['actor_id', 'director_id'])
 
